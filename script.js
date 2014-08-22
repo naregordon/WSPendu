8e4eb34586f59595a6e464297af8c8c6a1b5874f
Shadownest <pasccal.charreix@gmail.com>
2014-08-22 15:13:08 +0200

    playerlist + choose + sendkey

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-$(document).ready(function(){
-	var socket = io('192.168.1.93:8888');
+var socket;
+var login;
 
-	var login = prompt('quel est votre nom ?');
+function managePlayerList(list)
+{
+	var i = 0;
+	$(".other").empty();
+	while(list[i] != undefined)
+	{
+		$(".other").append("<h2>"+list[i]['login']+"</h2>");
+		i++;
+	}
+}
 
+function chooseWord()
+{
+	var word;
+	while (word.length < 4)
+		word = prompt("Choose your destiny !\n...\nAnd a word to find.");
+	socket.emit("word", word);
+}
+
+function sendKey(info)
+{
+	if (info.keyCode > 64 && info.keyCode < 90)
+		socket.emit("", String.fromCharCode(info.keyCode));
+}
+
+$(document).ready(function()
+{
+	socket = io('192.168.1.93:8888');
+	login = prompt('What is your nickname ? :)');
 	socket.emit("login", login);
 
-	socket.on("new_user", function(user){
-		$('#user').html('Bonjour ' + user);
-	});
+	socket.on("playerList", managePlayerList);
+	socket.on("firstPlayer", chooseWord);
 
-	socket.on("playerList", function(playerList) {
-		var i = 0;
-		console.log(playerList);
-		$(".other").empty();
-		while(i < playerList.length) {
-			$(".other").append("<h2>"+playerList[i]['login']+"</h2>");
-			i++;
-		}
-	})
+	$('body').on('keydown', sendKey);
 });
+
